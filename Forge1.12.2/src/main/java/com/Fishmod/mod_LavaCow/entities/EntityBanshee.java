f9fe96d3ae501900a6ccf317b262a80884d3b3a6
FuGu0416 <fish710178@gmail.com>
2020-08-04 00:25:57 +0800

    Banshee update & en_us grammer

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/entities/EntityBanshee.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/entities/EntityBanshee.java
@@ -1,18 +1,22 @@
 package com.Fishmod.mod_LavaCow.entities;
 
+import java.util.List;
 import java.util.Random;
 
 import javax.annotation.Nullable;
 
 import com.Fishmod.mod_LavaCow.mod_LavaCow;
 import com.Fishmod.mod_LavaCow.client.Modconfig;
-import com.Fishmod.mod_LavaCow.entities.EntityFoglet.AICastingApell;
+import com.Fishmod.mod_LavaCow.init.ModPotions;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityAreaEffectCloud;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIFleeSun;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
@@ -21,12 +25,16 @@ import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
-import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
@@ -39,8 +47,10 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EntityBanshee extends EntityMob implements IAggressive{
 	
+	private static final DataParameter<Byte> CASTING = EntityDataManager.<Byte>createKey(EntityBanshee.class, DataSerializers.BYTE);
 	private boolean isAggressive = false;
 	private int attackTimer = 0;
+	protected int spellTicks;
 	
 	public EntityBanshee(World worldIn)
     {
@@ -51,7 +61,9 @@ public class EntityBanshee extends EntityMob implements IAggressive{
     protected void initEntityAI()
     {
         this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(3, new EntityAIAttackMelee(this, 1.0D, false));
+        this.tasks.addTask(2, new AICastingApell());
+        this.tasks.addTask(3, new EntityBanshee.AIUseSpell());
+        this.tasks.addTask(4, new EntityAIAttackMelee(this, 1.0D, false));
         if(!Modconfig.SunScreen_Mode)this.tasks.addTask(5, new EntityAIFleeSun(this, 1.0D));
         this.tasks.addTask(6, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
@@ -85,6 +97,23 @@ public class EntityBanshee extends EntityMob implements IAggressive{
 	
     protected void entityInit() {
     	super.entityInit();
+    	this.dataManager.register(CASTING, Byte.valueOf((byte)0));
+    }
+    
+    public boolean isSpellcasting()
+    {
+    	return (((Byte)this.dataManager.get(CASTING)).byteValue() & 1) != 0;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public boolean isSpellcastingC()
+    {
+    	return (((Byte)this.dataManager.get(CASTING)).byteValue() & 1) != 0;
+    }
+    
+    public int getSpellTicks()
+    {
+        return this.spellTicks;
     }
 	
     /**
@@ -106,6 +135,11 @@ public class EntityBanshee extends EntityMob implements IAggressive{
     @Override
     public void onLivingUpdate()
     {
+        if (this.spellTicks > 0)
+        {
+            --this.spellTicks;
+        }
+    	
     	if (!Modconfig.SunScreen_Mode && this.world.isDaytime() && !this.world.isRemote)
     	{
     		float f = this.getBrightness();
@@ -200,6 +234,168 @@ public class EntityBanshee extends EntityMob implements IAggressive{
             super.handleStatusUpdate(id);
         }
     }
+    
+    public void setIsCasting(boolean isHanging)
+    {
+        byte b0 = ((Byte)this.dataManager.get(CASTING)).byteValue();
+
+        if (isHanging)
+        {
+            this.dataManager.set(CASTING, Byte.valueOf((byte)(b0 | 1)));
+        }
+        else
+        {
+            this.dataManager.set(CASTING, Byte.valueOf((byte)(b0 & -2)));
+        }
+    }
+    
+    public class AICastingApell extends EntityAIBase
+    {
+        public AICastingApell()
+        {
+            this.setMutexBits(3);
+        }
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            return EntityBanshee.this.getSpellTicks() > 0;
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting()
+        {
+            super.startExecuting();
+            EntityBanshee.this.setIsCasting(true);
+            EntityBanshee.this.navigator.clearPath();
+        }
+
+        /**
+         * Reset the task's internal state. Called when this task is interrupted by another one
+         */
+        public void resetTask()
+        {
+            super.resetTask();
+            EntityBanshee.this.setIsCasting(false);
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask()
+        {
+            if (EntityBanshee.this.getAttackTarget() != null)
+            {
+                EntityBanshee.this.getLookHelper().setLookPositionWithEntity(EntityBanshee.this.getAttackTarget(), (float)EntityBanshee.this.getHorizontalFaceSpeed(), (float)EntityBanshee.this.getVerticalFaceSpeed());
+            }
+        }
+    }
+    
+    public class AIUseSpell extends EntityAIBase
+    {
+        protected int spellWarmup;
+        protected int spellCooldown;
+
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        public boolean shouldExecute()
+        {
+            if (EntityBanshee.this.getAttackTarget() == null)
+            {
+                return false;
+            }
+            else if (EntityBanshee.this.isSpellcasting())
+            {
+                return false;
+            }
+            else
+            {
+            	return EntityBanshee.this.ticksExisted >= this.spellCooldown && EntityBanshee.this.getDistance(EntityBanshee.this.getAttackTarget()) < 3.0F;
+            }
+        }
+
+        /**
+         * Returns whether an in-progress EntityAIBase should continue executing
+         */
+        public boolean shouldContinueExecuting()
+        {
+            return EntityBanshee.this.getAttackTarget() != null && this.spellWarmup > 0;
+        }
+
+        /**
+         * Execute a one shot task or start executing a continuous task
+         */
+        public void startExecuting()
+        {
+            this.spellWarmup = this.getCastWarmupTime();
+            EntityBanshee.this.spellTicks = this.getCastingTime();
+            EntityBanshee.this.world.setEntityState(EntityBanshee.this, (byte)10);
+            this.spellCooldown = EntityBanshee.this.ticksExisted + this.getCastingInterval();
+            SoundEvent soundevent = this.getSpellPrepareSound();
+
+            if (soundevent != null)
+            {
+                EntityBanshee.this.playSound(soundevent, 1.0F, 1.0F);
+            }
+        }
+
+        /**
+         * Keep ticking a continuous task that has already been started
+         */
+        public void updateTask()
+        {
+            --this.spellWarmup;
+
+            if (this.spellWarmup == 0)
+            {
+                this.castSpell();
+                EntityBanshee.this.playSound(EntityBanshee.this.getSpellSound(), 1.0F, 1.0F);
+                EntityBanshee.this.world.setEntityState(EntityBanshee.this, (byte)11);
+            }
+        }
+
+        protected void castSpell()
+        {
+        	List<Entity> list = EntityBanshee.this.world.getEntitiesWithinAABBExcludingEntity(EntityBanshee.this, EntityBanshee.this.getEntityBoundingBox().grow(3.0D, 3.0D, 3.0D));
+
+        	for (Entity entity1 : list)
+        	{
+        		if (entity1 instanceof EntityLivingBase)
+        		{
+        			float local_difficulty = EntityBanshee.this.world.getDifficultyForLocation(new BlockPos(EntityBanshee.this)).getAdditionalDifficulty();
+                        
+        			if (((EntityLivingBase)entity1).getActivePotionEffect(MobEffects.WEAKNESS) == null)
+        				((EntityLivingBase)entity1).addPotionEffect(new PotionEffect(MobEffects.WEAKNESS, 8 * 20 * (int)local_difficulty, 2));
+        		}
+        	} 
+        }
+
+        protected int getCastWarmupTime()
+        {
+            return 20;
+        }
+
+        protected int getCastingTime()
+        {
+            return 30;
+        }
+
+        protected int getCastingInterval()
+        {
+            return 200;
+        }
+
+        @Nullable
+        protected SoundEvent getSpellPrepareSound()
+        {
+            return SoundEvents.EVOCATION_ILLAGER_PREPARE_ATTACK;
+        }
+    }
 
     public float getEyeHeight()
     {
@@ -219,6 +415,11 @@ public class EntityBanshee extends EntityMob implements IAggressive{
     protected SoundEvent getDeathSound()
     {
         return SoundEvents.ENTITY_BLAZE_DEATH;
+    }
+    
+    protected SoundEvent getSpellSound()
+    {
+        return SoundEvents.EVOCATION_ILLAGER_CAST_SPELL;
     }
     
     /**
