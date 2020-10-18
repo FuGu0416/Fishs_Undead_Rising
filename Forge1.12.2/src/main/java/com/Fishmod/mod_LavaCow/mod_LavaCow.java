9e571e2bdbd2642f1e260b10f00b1c213182abf4
FuGu0416 <fish710178@gmail.com>
2020-10-18 17:23:27 +0800

    1.2.1 & update lang

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/mod_LavaCow.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/mod_LavaCow.java
@@ -33,7 +33,7 @@ public class mod_LavaCow {
  
     public static final String MODID = "mod_lavacow";
     public static final String NAME = "Fish's Undead Rising";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
     public static final String MC_VERSION = "[1.12.2]";
     
     public static final String CLIENT = "com.Fishmod.mod_LavaCow.proxy.ClientProxy";
