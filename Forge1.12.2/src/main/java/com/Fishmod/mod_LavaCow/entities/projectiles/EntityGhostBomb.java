6ec79a7842d8eb043707217b64b2656ebaba5a5f
Avlyssna <Avlyssna@users.noreply.github.com>
2021-01-09 18:54:03 -0600

    Adjust grenade weight and remove needless entity
    - Adjusted the grenade weight to feel better when thrown
    - Removed a needless entity which was being created during the explosion process

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/entities/projectiles/EntityGhostBomb.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/entities/projectiles/EntityGhostBomb.java
@@ -1,10 +1,8 @@
 package com.Fishmod.mod_LavaCow.entities.projectiles;
 
-import com.Fishmod.mod_LavaCow.entities.EntityFoglet;
 import com.Fishmod.mod_LavaCow.init.FishItems;
 
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
@@ -12,7 +10,7 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class EntityGhostBomb extends EntityThrowable {
-	
+
     public EntityGhostBomb(World worldIn)
     {
         super(worldIn);
@@ -37,14 +35,11 @@ public class EntityGhostBomb extends EntityThrowable {
     }
 
 	@Override
-	protected void onImpact(RayTraceResult result) { 	
+	protected void onImpact(RayTraceResult result) {
         if (!this.world.isRemote)
         {
-        	EntityFoglet Dummy = new EntityFoglet(this.world);
-        	Dummy.setCustomNameTag("Ghost Bomb");
-        	this.world.createExplosion(Dummy, this.posX, this.posY, this.posZ, 4.0F, false);
-        	Dummy.setDead();
-        	this.world.playSound((EntityPlayer)null, new BlockPos(this.posX, this.posY, this.posZ), FishItems.ENTITY_BANSHEE_HURT, SoundCategory.BLOCKS, 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);       	
+        	this.world.createExplosion(null, this.posX, this.posY, this.posZ, 4.0F, false);
+        	this.world.playSound(null, new BlockPos(this.posX, this.posY, this.posZ), FishItems.ENTITY_BANSHEE_HURT, SoundCategory.BLOCKS, 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
         	this.setDead();
         }
 	}
