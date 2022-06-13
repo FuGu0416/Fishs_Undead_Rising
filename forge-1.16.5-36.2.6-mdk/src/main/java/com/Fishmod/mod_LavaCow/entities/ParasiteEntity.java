8590d2fb17274015b8847811823631f40c5e47b3
FuGu0416 <fish710178@gmail.com>
2022-06-14 00:27:00 +0800

    parasite fix

--- forge-1.16.5-36.2.6-mdk/src/main/java/com/Fishmod/mod_LavaCow/entities/ParasiteEntity.java
+++ forge-1.16.5-36.2.6-mdk/src/main/java/com/Fishmod/mod_LavaCow/entities/ParasiteEntity.java
@@ -64,7 +64,7 @@ public class ParasiteEntity extends SpiderEntity {
 	public ParasiteEntity(EntityType<? extends ParasiteEntity> p_i48549_1_, World worldIn) {
         super(p_i48549_1_, worldIn);
         this.long_live = false;
-        this.lifespawn = 16*20;//Can live for only 16secs, poor little one:(
+        this.lifespawn = 16*20; // Can live for 16s only, poor little one :(
         this.xpReward = 1;
     }
 	
@@ -116,7 +116,9 @@ public class ParasiteEntity extends SpiderEntity {
 	@Override
 	public void tick() {
         if (this.lifespawn > 0) {
-        	if(!long_live)this.lifespawn--;
+        	if(!long_live) {
+        		this.lifespawn--;
+        	}
         } else if (this.getSkin() == 2 && this.getRandom().nextInt(100) < FURConfig.pSpawnRate_Vespa.get()) {
         	double d0 = this.getAttributeValue(Attributes.FOLLOW_RANGE);
         	List<PlayerEntity> list = this.level.getEntitiesOfClass(PlayerEntity.class, this.getBoundingBox().inflate(d0));
@@ -130,7 +132,8 @@ public class ParasiteEntity extends SpiderEntity {
 		    		VespaCocoonEntity pupa = FUREntityRegistry.VESPACOCOON.create(this.level);
 		    		pupa.moveTo(this.getX(), this.getY(), this.getZ(), this.yRot, this.xRot);
 		    		this.level.addFreshEntity(pupa);
-        		}     		
+        		}   
+        		
         		this.remove();
         	} else
         		this.hurt(DamageSource.mobAttack(this).bypassInvul().bypassArmor() , this.getMaxHealth());
@@ -150,8 +153,10 @@ public class ParasiteEntity extends SpiderEntity {
         	} else if (mount.isAlive() && this.tickCount % 20 == 0) {
         		this.doHurtTarget(mount);
         	}
-        } else if (getVehicle() == null && this.long_live)
+        	
+        } else if (getVehicle() == null && this.long_live) {
         	this.long_live = false;
+        }
              
         if (!this.level.isClientSide()) {
             if (this.isOnGround() || this.isInWaterOrBubble() || this.isInLava()) {
@@ -161,6 +166,7 @@ public class ParasiteEntity extends SpiderEntity {
             } else {
                 Direction closestDirection = Direction.DOWN;
                 double closestDistance = 100;
+                
                 for (Direction dir : DIRECTIONS) {
                     BlockPos antPos = new BlockPos(Math.floor(this.getX()), Math.floor(this.getY()), Math.floor(this.getZ()));
                     BlockPos offsetPos = antPos.relative(dir);
@@ -170,6 +176,7 @@ public class ParasiteEntity extends SpiderEntity {
                         closestDirection = dir;
                     }
                 }
+                
                 this.entityData.set(ATTACHED_BLK, closestDirection);
             }
         }      
@@ -186,6 +193,7 @@ public class ParasiteEntity extends SpiderEntity {
         	if (!player.inventory.add(new ItemStack(ParasiteDrop[this.getSkin()]))) {
                 player.spawnAtLocation(new ItemStack(ParasiteDrop[this.getSkin()]));
             }
+        	
         	this.remove();
         	
         	return ActionResultType.SUCCESS;
@@ -195,40 +203,46 @@ public class ParasiteEntity extends SpiderEntity {
     }
 	
 	@Override
-	public double getMyRidingOffset() {
-		if (this.getVehicle() != null && (this.getVehicle() instanceof PlayerEntity || this.getVehicle() instanceof ZombieEntity))
-			return this.getVehicle().getBbHeight()/2  - 0.85F;
-		else if (this.getVehicle() != null)
-			return this.getVehicle().getBbHeight() * 0.65D - 1.0D;
-		else
+	public double getMyRidingOffset() {		
+		if (this.isPassenger()) {
+			if (this.getVehicle() instanceof PlayerEntity || this.getVehicle() instanceof ZombieEntity) {
+				return this.getVehicle().getBbHeight()/2  - 0.85F;
+			} else {
+				return this.getVehicle().getBbHeight() * 0.65D - 1.0D;
+			}
+		} else {
 			return super.getMyRidingOffset();
+		}
 	}
 	
 	@Override
 	public boolean doHurtTarget(Entity entity) {
 		if (super.doHurtTarget(entity)) {
-			((LivingEntity) entity).addEffect(new EffectInstance(FUREffectRegistry.INFESTED, 8*20, 0));
-			if(this.getSkin() == 2)
+			((LivingEntity) entity).addEffect(new EffectInstance(FUREffectRegistry.INFESTED, 8*20, 0));			
+			if(this.getSkin() == 2) {
 				((LivingEntity) entity).addEffect(new EffectInstance(Effects.POISON, 4*20, 0));
+			}
+			
 			return true;
 		}
+		
 		return false;
 	}
 	
 	@Override
     public void push(Entity entityIn) {		
-		if (entityIn instanceof LivingEntity && LootTableHandler.PARASITE_HOSTLIST.contains(entityIn.getType().getRegistryName()) && FURConfig.Parasite_Attach.get()) {
+		super.push(entityIn);
+		if (entityIn instanceof LivingEntity && !(entityIn instanceof PlayerEntity) && LootTableHandler.PARASITE_HOSTLIST.contains(entityIn.getType().getRegistryName()) && FURConfig.Parasite_Attach.get() && !this.isPassenger()) {
     		((LivingEntity) entityIn).addEffect(new EffectInstance(FUREffectRegistry.INFESTED, 8*20, 0));
     		this.startRiding(entityIn);
             this.long_live = true;
-        } else
-        	super.push(entityIn);
+        }
     }
 	
 	@Override
 	public void playerTouch(PlayerEntity playerIn) {
 		super.playerTouch(playerIn);
-		if (!playerIn.isCreative() && FURConfig.Parasite_Attach.get()) {
+		if (!playerIn.isCreative() && FURConfig.Parasite_Attach.get() && !this.isPassenger()) {
 			playerIn.addEffect(new EffectInstance(FUREffectRegistry.INFESTED, 8*20, 0));
     		this.startRiding(playerIn);
             this.long_live = true;
@@ -237,7 +251,9 @@ public class ParasiteEntity extends SpiderEntity {
 	
 	public static ParasiteEntity gotParasite(List<Entity> listIn) {
 		for(Entity C : listIn)
-			if(C instanceof ParasiteEntity)return (ParasiteEntity)C;		
+			if(C instanceof ParasiteEntity)
+				return (ParasiteEntity)C;	
+		
 		return null;
 	}
 	
