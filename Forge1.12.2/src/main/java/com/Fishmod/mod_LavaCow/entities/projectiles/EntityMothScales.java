7beb680e95918e73ef3e02a5731c41ff904b4870
Me <135455255+IcarussOne@users.noreply.github.com>
2025-01-23 05:40:37 -0600

    Add config option to control enigmoth scale lingering cloud time
    Also slightly reduced the amount of seconds needed before it despawns.
    
    #193

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/entities/projectiles/EntityMothScales.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/entities/projectiles/EntityMothScales.java
@@ -2,6 +2,8 @@ package com.Fishmod.mod_LavaCow.entities.projectiles;
 
 import com.Fishmod.mod_LavaCow.init.ModMobEffects;
 import com.Fishmod.mod_LavaCow.mod_LavaCow;
+import com.Fishmod.mod_LavaCow.client.Modconfig;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAreaEffectCloud;
 import net.minecraft.entity.EntityLiving;
@@ -110,7 +112,7 @@ public class EntityMothScales extends EntityFireball {
                             entityareaeffectcloud.setOwner(owner);
                             entityareaeffectcloud.setRadius(2.0F);
                             entityareaeffectcloud.setRadiusOnUse(-0.5F);
-                            entityareaeffectcloud.setWaitTime(10);
+                            entityareaeffectcloud.setWaitTime(Modconfig.Enigmoth_Scale_Potion_Cloud_Time);
                             entityareaeffectcloud.setDuration(entityareaeffectcloud.getDuration() / 2);
                             entityareaeffectcloud.setRadiusPerTick(-entityareaeffectcloud.getRadius() / (float) entityareaeffectcloud.getDuration());
 
