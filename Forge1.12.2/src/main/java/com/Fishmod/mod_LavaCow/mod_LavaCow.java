50d5a1e8897bbe1295e2dbdac55159b756d68e9b
Me <135455255+IcarussOne@users.noreply.github.com>
2025-01-23 03:56:41 -0600

    Fix forgotten loot table addition to enigmoth

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/mod_LavaCow.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/mod_LavaCow.java
@@ -40,7 +40,7 @@ public class mod_LavaCow {
 
     public static final String MODID = "mod_lavacow";
     public static final String NAME = "Fish's Undead Rising";
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
     public static final String MC_VERSION = "[1.12.2]";
     public static final String DEPENDENCIES = "after:tconstruct;after:conarm";
 
