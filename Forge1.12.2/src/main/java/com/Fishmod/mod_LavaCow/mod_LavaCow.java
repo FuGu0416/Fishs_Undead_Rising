5e2db7e47958ce28a2a8d1d5261a7635eb479f57
FuGu0416 <fish710178@gmail.com>
2025-12-13 16:09:17 +0800

    20251213 update to 1.5.3

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/mod_LavaCow.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/mod_LavaCow.java
@@ -7,7 +7,6 @@ import com.Fishmod.mod_LavaCow.compat.somanyenchantments.SoManyEnchantmentsCompa
 import com.Fishmod.mod_LavaCow.compat.tinkers.ConstructsArmoryCompat;
 import com.Fishmod.mod_LavaCow.compat.tinkers.TinkersCompat;
 import com.Fishmod.mod_LavaCow.compat.tinkers.TinkersCompatClient;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -45,7 +44,7 @@ public class mod_LavaCow {
 
     public static final String MODID = "mod_lavacow";
     public static final String NAME = "Fish's Undead Rising";
-    public static final String VERSION = "1.5.2";
+    public static final String VERSION = "1.5.3";
     public static final String MC_VERSION = "[1.12.2]";
     public static final String DEPENDENCIES = "after:tconstruct;after:conarm";
 
