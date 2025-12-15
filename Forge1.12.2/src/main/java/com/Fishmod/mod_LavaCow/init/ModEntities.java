7737d72da82aaa2488016ab7b9574cd5acc145e4
Me <135455255+IcarussOne@users.noreply.github.com>
2025-12-15 09:25:57 -0600

    Comment out the correct debug code

--- Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/init/ModEntities.java
+++ Forge1.12.2/src/main/java/com/Fishmod/mod_LavaCow/init/ModEntities.java
@@ -525,13 +525,13 @@ public class ModEntities {
         @SubscribeEvent
         public static void onEvent(final RegistryEvent.Register<EntityEntry> event) {
             // DEBUG
-            /*System.out.println("Registering entities");
+            //System.out.println("Registering entities");
             for (final EntityEntry entityEntry : SET_ENTITIES) {
                 // DEBUG
-                System.out.println("Registering entity = " + entityEntry.getEntityClass());
+                //System.out.println("Registering entity = " + entityEntry.getEntityClass());
                 IForgeRegistry<EntityEntry> registry = event.getRegistry();
                 registry.register(entityEntry);
-            }*/
+            }
 
             RegisterEntitySpawn();
         }
